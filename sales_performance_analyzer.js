dbad05ee09c61a1cd7cd68af5cadd1cf9e776144
tcoolican <tcoolican@usf.edu>
2024-09-08 19:57:43 -0400

    Create a Function to Determine Performance Rating

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -3,11 +3,30 @@ function calculateAverageSales(salesFigures) {
     if (salesFigures.length === 0) {
       return 0;
     }
-    const sum = salesFigures.reduce((acc, current) => acc + current, 0);
+    const sum = salesFigures.reduce((acc, sale) => acc + sale, 0);
     const average = sum / salesFigures.length;
     return average;
   }
   const salesFigures = [100, 200, 300, 400, 500];
 const averageSales = calculateAverageSales(salesFigures);
 console.log(averageSales);
+
+//Create a Function to Determine Performance Rating
+function determinePerformanceRating(averageSale) {
+    if (averageSale > 10000) {
+      return "Excellent";
+    }
+    else if (averageSale >= 7000 && averageSale <= 10000) {
+      return "Good";
+    }
+    else if (averageSale >= 4000 && averageSale < 7000) {
+      return "Satisfactory";
+    }
+    else {
+      return "Needs Improvement";
+    }
+  }
+  const averageSale = 12000;
+const performanceRating = determinePerformanceRating(averageSale);
+console.log(performanceRating);
 
